b00a13be7a94af2ce8431ac6c28f8103c6c58dc3
Eremeow138 <eremeow138@yandex.ru>
2021-10-08 14:54:33 +0800

    refactor: correct footer's styles, markup and logic

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
-  public copyright = 'Copyright © Videocourses. All rights reserved';
-}
+export class FooterComponent {}
