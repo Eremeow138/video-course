a65b5f26b78b0658588b88397d30a5ea2b507c6d
Eremeow138 <eremeow138@yandex.ru>
2021-10-28 12:46:03 +0800

    refactor: rename courses-page component to courses-list-page component

--- src/app/pages/courses-page/pages/courses-list-page/components/courses-list-page.component.spec.ts
+++ src/app/pages/courses-page/pages/courses-list-page/components/courses-list-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CoursesPageComponent } from './courses-page.component';
+import { CoursesPageComponent } from './courses-list-page.component';
 
 describe('CoursesPageComponent', () => {
   let component: CoursesPageComponent;
--- src/app/pages/courses-page/pages/courses-list-page/courses-list-page.module.ts
+++ src/app/pages/courses-page/pages/courses-list-page/courses-list-page.module.ts
@@ -5,13 +5,13 @@ import { ActionsComponent } from './components/actions/actions.component';
 import { CourseActionsComponent } from './components/courses-list/course-card/course-actions/course-actions.component';
 import { CourseCardComponent } from './components/courses-list/course-card/course-card.component';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
-import { CoursesPageComponent } from './components/courses-page.component';
+import { CoursesListPageComponent } from './components/courses-list-page.component';
 import { CourseToolsComponent } from './components/courses-list/course-card/course-tools/course-tools.component';
 import { DurationPipe } from './pipes/duration.pipe';
 import { FiltersComponent } from './components/filters/filters.component';
 import { SearchComponent } from './components/filters/search/search.component';
 
-const COMPONENTS = [CoursesPageComponent];
+const COMPONENTS = [CoursesListPageComponent];
 
 @NgModule({
   declarations: [
@@ -23,7 +23,6 @@ const COMPONENTS = [CoursesPageComponent];
     CourseCardComponent,
     CourseToolsComponent,
     CourseActionsComponent,
-    CoursesPageComponent,
     DurationPipe,
   ],
   imports: [CommonModule, FormsModule],
