bfcb79a5bf3bc61fff09e8a6fe7f11ed2481478d
Eremeow138 <eremeow138@yandex.ru>
2021-11-11 15:25:26 +0800

    feat: add top rated field using ngif and ngclass

--- src/app/pages/courses-page/pages/courses-list-page/components/courses-list/courses-list.component.ts
+++ src/app/pages/courses-page/pages/courses-list-page/components/courses-list/courses-list.component.ts
@@ -14,16 +14,18 @@ export class CoursesListComponent implements OnInit {
       {
         id: 1,
         title: "Video Course 1. Name tag",
-        creationDate: "2021-09-09T04:39:24+00:00",
+        creationDate: "2021-11-05T04:39:24+00:00",
         duration: 88,
         description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.`,
+        isTopRated: true,
       },
       {
         id: 2,
         title: "Video Course 2. Name tag",
-        creationDate: "2021-09-09T04:39:24+00:00",
+        creationDate: "2021-12-12T04:39:24+00:00",
         duration: 34,
         description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.`,
+        isTopRated: false,
       },
       {
         id: 3,
@@ -31,6 +33,7 @@ export class CoursesListComponent implements OnInit {
         creationDate: "2021-09-09T04:39:24+00:00",
         duration: 120,
         description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.`,
+        isTopRated: false,
       },
     ];
   }
@@ -49,6 +52,7 @@ export class CoursesListComponent implements OnInit {
         creationDate: "2021-09-09T04:39:24+00:00",
         duration: 1250,
         description: `Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.`,
+        isTopRated: true,
       },
     ];
   }
