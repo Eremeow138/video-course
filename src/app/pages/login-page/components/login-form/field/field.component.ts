b3e7f93b8f02f850bf5aa31729cdc44347ee74e1
Eremeow138 <eremeow138@yandex.ru>
2021-12-07 17:28:12 +0800

    feat: make field as component

---
+++ src/app/pages/login-page/components/login-form/field/field.component.ts
@@ -0,0 +1,24 @@
+import { AfterContentInit, Component, ContentChild, ElementRef, Input } from "@angular/core";
+
+@Component({
+  selector: "app-field",
+  templateUrl: "./field.component.html",
+  styleUrls: ["./field.component.scss"]
+})
+export class FieldComponent implements AfterContentInit {
+
+
+  @Input()
+  public title!: string;
+
+  public isRequired = false;
+
+  @ContentChild("content")
+  private contentRef!: ElementRef;
+
+  ngAfterContentInit() {
+    if (this.contentRef.nativeElement.required) {
+      this.isRequired = true;
+    }
+  }
+}
--- src/app/pages/login-page/login-page.module.ts
+++ src/app/pages/login-page/login-page.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 import { LoginPageComponent } from "./components/login-page.component";
-import { LoginFormComponent } from "./components/login/login-form.component";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { FieldComponent } from "./components/login-form/field/field.component";
 
 const COMPONENTS = [LoginPageComponent];
 
 @NgModule({
   declarations: [
     ...COMPONENTS,
-    LoginFormComponent
+    LoginFormComponent,
+    FieldComponent
   ],
   imports: [
     CommonModule,
