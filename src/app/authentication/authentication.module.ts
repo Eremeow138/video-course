7d971db4273b175525d6c9c7918c68c9b9b2d99d
Eremeow138 <eremeow138@yandex.ru>
2021-12-11 09:44:22 +0800

    fix: remove ifAuthenticated directive

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { IfAuthenticatedDirective } from "./directives/if-authenticated/if-authenticated.directive";
-
 @NgModule({
-  declarations: [IfAuthenticatedDirective],
+  declarations: [],
   imports: [
     CommonModule
   ],
-  exports: [IfAuthenticatedDirective]
 })
 export class AuthenticationModule { }
