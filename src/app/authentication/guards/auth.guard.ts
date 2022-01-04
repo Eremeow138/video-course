99b212714c23d81f9b5b65933d307eae121e208a
Eremeow138 <eremeow138@yandex.ru>
2022-01-04 16:15:45 +0800

    refactor: simplify code in auth.guard

--- src/app/authentication/guards/auth.guard.ts
+++ src/app/authentication/guards/auth.guard.ts
@@ -1,56 +1,38 @@
-import { Injectable, OnDestroy } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, UrlTree } from "@angular/router";
 import { RouterPath } from "@commons/enums/router-path.enum";
 import { AuthService } from "@authentication/services/auth/auth.service";
-import { Observable, Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
-export class AuthGuard implements CanActivate, CanActivateChild, OnDestroy {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
-  private isAuthenticated = false;
+  constructor(private authService: AuthService, private router: Router) { }
 
-  private unsubscribe$ = new Subject<void>();
-
-  constructor(private authService: AuthService, private router: Router) {
-    this.subscribeToAuthentication();
-  }
-
-  public ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
-
-  public canActivate(route: ActivatedRouteSnapshot,): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  public canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     const isLoginPath = route.routeConfig.path === RouterPath.LoginPage;
 
-    if (this.isAuthenticated && isLoginPath) {
-      return this.router.parseUrl(RouterPath.CoursesPage);
-    }
-
-    if (this.isAuthenticated && !isLoginPath) {
-      return true;
-    }
-
-    if (!this.isAuthenticated && isLoginPath) {
-      return true;
-    }
-
-    return this.router.parseUrl(RouterPath.LoginPage);
+    return this.authService.isAuthenticated$.pipe(
+      map(isAuth => {
+        if (isAuth && isLoginPath) {
+          return this.router.parseUrl(RouterPath.CoursesPage);
+        }
+        if (isAuth && !isLoginPath) {
+          return true;
+        }
+        if (!isAuth && isLoginPath) {
+          return true;
+        }
+        return this.router.parseUrl(RouterPath.LoginPage);
+      })
+    );
   }
 
   public canActivateChild(next: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.canActivate(next);
-  }
-
-  private subscribeToAuthentication(): void {
-    this.authService.isAuthenticated$.pipe(
-      takeUntil(this.unsubscribe$)
-    ).subscribe(isAuthenticated => {
-      this.isAuthenticated = isAuthenticated;
-    });
   }
 }
