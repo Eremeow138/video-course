feadcb763d35bd1fa40bd48e440fd5050dbd1834
Eremeow138 <eremeow138@yandex.ru>
2022-01-04 10:59:32 +0800

    refactor: remove empty object from router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     AuthenticationModule,
-    RouterModule.forRoot(routes, {})
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
 })
