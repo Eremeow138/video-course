8cef4e480d6c6b2821319f07365495c1b7e892d8
Eremeow138 <eremeow138@yandex.ru>
2022-01-31 10:31:53 +0800

    fix: correct type of duration field

--- src/app/form/types/course-form-value.ts
+++ src/app/form/types/course-form-value.ts
@@ -3,7 +3,7 @@ import { CourseFormControl } from "../enums/course-form-control.enum";
 export type CourseFormValue = {
   [CourseFormControl.Title]: string;
   [CourseFormControl.Description]: string;
-  [CourseFormControl.Duration]: string;
+  [CourseFormControl.Duration]: number;
   [CourseFormControl.Date]: string;
   [CourseFormControl.Authors]: string[];
 };
