0c16247eeaabc3d4574649cfc5b93faa1ca2c0bf
Eremeow138 <eremeow138@yandex.ru>
2022-01-31 10:36:31 +0800

    feat: add resolving page logic for course-page

--- src/app/pages/courses-page/pages/course-page/components/course-form/course-form.component.ts
+++ src/app/pages/courses-page/pages/course-page/components/course-form/course-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { CourseFormControl } from "@app/form/enums/course-form-control.enum";
 import { CourseForm } from "@app/form/models/course-form.model";
+import { ICourse } from "@pages/courses-page/courses/interfaces/course.interface";
 
 @Component({
   selector: "app-course-form",
@@ -8,6 +9,17 @@ import { CourseForm } from "@app/form/models/course-form.model";
   styleUrls: ["./course-form.component.scss"]
 })
 export class CourseFormComponent implements OnInit {
+
+  @Input()
+  public course: ICourse = {
+    id: null,
+    title: null,
+    description: null,
+    duration: null,
+    creationDate: null,
+    authors: null,
+    isTopRated: false
+  };
 
   public titleFormControlName = CourseFormControl.Title;
   public descriptionFormControlName = CourseFormControl.Description;
@@ -25,11 +37,11 @@ export class CourseFormComponent implements OnInit {
 
   public createForm(): void {
     this.courseForm = new CourseForm({
-      [CourseFormControl.Title]: "",
-      [CourseFormControl.Description]: "",
-      [CourseFormControl.Duration]: "",
-      [CourseFormControl.Date]: "",
-      [CourseFormControl.Authors]: [],
+      [CourseFormControl.Title]: this.course.title,
+      [CourseFormControl.Description]: this.course.description,
+      [CourseFormControl.Duration]: this.course.duration,
+      [CourseFormControl.Date]: this.course.creationDate,
+      [CourseFormControl.Authors]: this.course.authors,
     });
   }
 
--- src/app/pages/courses-page/pages/course-page/components/course-page.component.ts
+++ src/app/pages/courses-page/pages/course-page/components/course-page.component.ts
@@ -1,12 +1,68 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { RouterPath } from "@commons/enums/routers.enum";
+import { ICourse } from "@pages/courses-page/courses/interfaces/course.interface";
+import { CoursesService } from "@pages/courses-page/courses/services/courses.service";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: "app-course-page",
   templateUrl: "./course-page.component.html",
   styleUrls: ["./course-page.component.scss"]
 })
-export class CoursePageComponent {
+export class CoursePageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public course: ICourse = {
+    id: null,
+    title: null,
+    description: null,
+    duration: null,
+    creationDate: null,
+    authors: null,
+    isTopRated: false
+  };
+
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(private route: ActivatedRoute, private router: Router, private coursesService: CoursesService) { }
+
+  public ngOnInit(): void {
+    this.subscribeToRoute();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+
+  private subscribeToRoute(): void {
+    this.route.params.pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe(routeParams => this.resolvePage(routeParams));
+  }
+
+  private resolvePage(routeParams: Params): void {
+    const courseId = Number(routeParams.id);
+
+    if (!isNaN(courseId)) {
+      const course = this.coursesService.getCourse(courseId);
+      if (course) {
+        this.course = course;
+        return;
+      }
+      this.navigateToCoursesListPage();
+      return;
+    }
+
+    if (routeParams.id && routeParams.id !== RouterPath.CourseCreationPage) {
+      this.navigateToCoursesListPage();
+    }
+  }
+
+  private navigateToCoursesListPage(): void {
+    this.router.navigateByUrl(`/${RouterPath.CoursesListPage}`);
+  }
 
 }
