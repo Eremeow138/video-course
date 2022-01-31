102a932121bfeac46f024e4c6948eded11867266
Eremeow138 <eremeow138@yandex.ru>
2022-01-31 10:34:23 +0800

    fix: correct setting of initial value for datepicker

--- src/app/form/components/field-types/datepicker-field/datepicker-field.component.ts
+++ src/app/form/components/field-types/datepicker-field/datepicker-field.component.ts
@@ -1,31 +1,38 @@
-import { ChangeDetectorRef, Component, ElementRef, OnInit, Renderer2, ViewChild } from "@angular/core";
+import { DatePipe } from "@angular/common";
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
 import { AbstractFieldComponent } from "../abstract-field/abstract-field.component";
 
 @Component({
   selector: "app-datepicker-field",
   templateUrl: "./datepicker-field.component.html",
-  styleUrls: ["./datepicker-field.component.scss"]
+  styleUrls: ["./datepicker-field.component.scss"],
+  providers: [DatePipe]
 })
-export class DatepickerFieldComponent extends AbstractFieldComponent implements OnInit {
+export class DatepickerFieldComponent extends AbstractFieldComponent implements AfterViewInit {
 
   @ViewChild("datePickerInput")
   public datePickerInput: ElementRef<HTMLInputElement>;
 
-  private userInput = "";
-
-  constructor(private renderer: Renderer2, cd: ChangeDetectorRef) {
+  constructor(private renderer: Renderer2, cd: ChangeDetectorRef, private datePipe: DatePipe) {
     super(cd);
   }
 
-  ngOnInit(): void {
-    this.control.valueChanges.subscribe(() => {
-      if (this.control.errors && this.control.errors.bsDate && this.control.errors.bsDate.invalid) {
-        this.renderer.setProperty(this.datePickerInput.nativeElement, "value", this.userInput);
-      }
-    });
+  ngAfterViewInit(): void {
+    const initDate = this.datePipe.transform(this.control.value, "MM/dd/yyyy");
+    this.renderer.setProperty(this.datePickerInput.nativeElement, "value", initDate);
   }
 
-  onDateChange(): void {
-    this.userInput = this.datePickerInput.nativeElement.value;
+  // TODO make it better
+  setDate(date: Date): void {
+    if (!isNaN(date.getTime())) {
+      const formatedDate = this.datePipe.transform(date, "yyyy-MM-ddThh:mm:sszzzz").replace("GMT", "");
+      this.control.setValue(formatedDate);
+      return;
+    }
+    this.control.setValue(date);
+  }
+
+  public touchControl(): void {
+    this.control.markAsTouched();
   }
 }
