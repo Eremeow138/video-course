c18d05ee93b2d0fa7d33af4f191d7d41413ba00e
Eremeow138 <eremeow138@yandex.ru>
2022-02-02 21:10:03 +0800

    feat: add select input type for string field

--- src/app/form/components/field-types/string-field/string-field.component.ts
+++ src/app/form/components/field-types/string-field/string-field.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { StringFieldInputType } from "@app/form/types/string-field-input-type.type";
 import { AbstractFieldComponent } from "../abstract-field/abstract-field.component";
 
 @Component({
@@ -6,5 +7,8 @@ import { AbstractFieldComponent } from "../abstract-field/abstract-field.compone
   templateUrl: "./string-field.component.html",
   styleUrls: ["./string-field.component.scss"],
 })
-export class StringFieldComponent extends AbstractFieldComponent { }
+export class StringFieldComponent extends AbstractFieldComponent {
+  @Input()
+  public inputType: StringFieldInputType = "text";
+}
 
