00f936926092d2fc9a9ed1ea144d0ba1b31e8d8f
Eremeow138 <eremeow138@yandex.ru>
2022-02-07 10:16:39 +0800

    refactor: add access modificators to custom route reuse strategy methods

--- src/app/custom-route-reuse-strategy.ts
+++ src/app/custom-route-reuse-strategy.ts
@@ -4,24 +4,24 @@ import { CoursesListPageComponent } from "@pages/courses-page/pages/courses-list
 export class CustomRouteReuseStrategy implements RouteReuseStrategy {
   private storedRoutes = new Map<string, DetachedRouteHandle>();
 
-  shouldDetach(route: ActivatedRouteSnapshot): boolean {
+  public shouldDetach(route: ActivatedRouteSnapshot): boolean {
     return this.getKey(route) === CoursesListPageComponent.toString();
 
   }
 
-  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+  public store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
     this.storedRoutes.set(this.getKey(route), handle);
   }
 
-  shouldAttach(route: ActivatedRouteSnapshot): boolean {
+  public shouldAttach(route: ActivatedRouteSnapshot): boolean {
     return !!route.routeConfig && !!this.storedRoutes.get(this.getKey(route));
   }
 
-  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+  public retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
     return this.storedRoutes.get(this.getKey(route));
   }
 
-  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+  public shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
     return future.routeConfig === curr.routeConfig;
   }
 
