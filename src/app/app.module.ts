ef3e01f5051fafb9396503b9ae88971e0d4b5b7b
Eremeow138 <eremeow138@yandex.ru>
2022-02-09 11:49:04 +0800

    refactor: change order of imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouteReuseStrategy } from "@angular/router";
+
 import { CommonsModule } from "./commons/commons.module";
 import { MarkupModule } from "./markup/markup.module";
 import { ModalsModule } from "./modals/modals.module";
 import { AuthenticationModule } from "./authentication/authentication.module";
-
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { RouteReuseStrategy } from "@angular/router";
+
+import { AppRoutingModule } from "./app-routing.module";
 import { CustomRouteReuseStrategy } from "@app/custom-route-reuse-strategy";
 
 @NgModule({
