8073fc8f4804a6cca3f75e58ed22a7737cbed7fb
Eremeow138 <eremeow138@yandex.ru>
2022-02-09 10:56:54 +0800

    fix: correct test for courses list page component

--- src/app/pages/courses-page/pages/courses-list-page/components/courses-list-page.component.spec.ts
+++ src/app/pages/courses-page/pages/courses-list-page/components/courses-list-page.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { CoursesPageComponent } from "@pages/courses-page/components/courses-page.component";
+import { CoursesListPageComponent } from "./courses-list-page.component";
 
 
-describe("CoursesPageComponent", () => {
-  let component: CoursesPageComponent;
-  let fixture: ComponentFixture<CoursesPageComponent>;
+describe("CoursesListPageComponent", () => {
+  let component: CoursesListPageComponent;
+  let fixture: ComponentFixture<CoursesListPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CoursesPageComponent],
+      declarations: [CoursesListPageComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CoursesPageComponent);
+    fixture = TestBed.createComponent(CoursesListPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
