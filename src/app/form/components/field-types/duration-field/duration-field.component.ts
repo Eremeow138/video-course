e6aa2c39ca77e198a0d89950474b0e994d7062ba
Eremeow138 <eremeow138@yandex.ru>
2022-02-13 13:27:35 +0800

    feat: add errors for duration field component

--- src/app/form/components/field-types/duration-field/duration-field.component.ts
+++ src/app/form/components/field-types/duration-field/duration-field.component.ts
@@ -10,4 +10,18 @@ import { AbstractFieldComponent } from "../abstract-field/abstract-field.compone
 export class DurationFieldComponent extends AbstractFieldComponent {
   public minValue = Duration.MinValue;
   public maxValue = Duration.MaxValue;
+
+  public get errors(): string[] {
+    const errors: string[] = [];
+    if (this.control && this.control.errors && (this.control.errors.max || this.control.errors.pattern)) {
+      errors.push("The maximum allowable value has been exceeded");
+    }
+    if (this.control && this.control.errors && this.control.errors.min) {
+      errors.push("The minimum allowable value has been exceeded");
+    }
+    if (this.control && this.control.errors && this.control.errors.pattern && !this.control.errors.max) {
+      errors.push("Not an integer has been entered");
+    }
+    return errors;
+  }
 }
