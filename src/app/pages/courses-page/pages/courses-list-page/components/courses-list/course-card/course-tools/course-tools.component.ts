808c6c2892bb275144127d51e4f206c4ebe5ef02
Eremeow138 <eremeow138@yandex.ru>
2022-03-10 19:34:47 +0800

    fix(course-tools): change default change detection strategy to onpush

--- src/app/pages/courses-page/pages/courses-list-page/components/courses-list/course-card/course-tools/course-tools.component.ts
+++ src/app/pages/courses-page/pages/courses-list-page/components/courses-list/course-card/course-tools/course-tools.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { ICourse } from "@pages/courses-page/courses/interfaces/course/course.interface";
 
 @Component({
   selector: "app-course-tools",
   templateUrl: "./course-tools.component.html",
   styleUrls: ["./course-tools.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseToolsComponent {
   @Input()
