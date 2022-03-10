2ad7def704a5d58122466d88ccc9a827360e8daf
Eremeow138 <eremeow138@yandex.ru>
2022-03-10 21:21:09 +0800

    fix(courses-page): change default change detection strategy to onpush

--- src/app/pages/courses-page/components/courses-page.component.ts
+++ src/app/pages/courses-page/components/courses-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-courses-page",
   templateUrl: "./courses-page.component.html",
-  styleUrls: ["./courses-page.component.scss"]
+  styleUrls: ["./courses-page.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CoursesPageComponent { }
