1414d30a513aab06b4318abb693f36286f28a169
Eremeow138 <eremeow138@yandex.ru>
2022-03-16 16:34:44 +0800

    fix(tag-field): add touchControl method inside removeTag method

--- src/app/form/components/field-types/tag-field/tag-field.component.ts
+++ src/app/form/components/field-types/tag-field/tag-field.component.ts
@@ -78,6 +78,7 @@ export class TagFieldComponent extends AbstractFieldComponent {
   public removeTag(removableTag: string): void {
     const tags = this.control.value as string[];
     this.control.patchValue([...tags.filter(tag => tag !== removableTag)]);
+    this.touchControl();
   }
 
   public touchControl(): void {
