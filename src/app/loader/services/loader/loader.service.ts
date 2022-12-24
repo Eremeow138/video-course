da71aca0d6aea08e4995851675b9c71db1436c49
Eremeow138 <eremeow138@yandex.ru>
2022-12-24 07:44:37 +0300

    fix: correct an loader logic

--- src/app/loader/services/loader/loader.service.ts
+++ src/app/loader/services/loader/loader.service.ts
@@ -1,26 +1,37 @@
-import { Injectable, OnDestroy } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { delay, switchMap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
-export class LoaderService implements OnDestroy {
+export class LoaderService {
+
+  private loadingSet = new Set<string>();
 
   private loaderStateSubject = new BehaviorSubject<boolean>(false);
 
-  ngOnDestroy(): void {
-    this.loaderStateSubject.complete();
+  setLoading(loading: boolean, url: string): void {
+    if (!url) {
+      throw new Error("The request URL must be provided to the LoadingService.setLoading function");
+    }
+
+    if (loading) {
+      this.loadingSet.add(url);
+      this.loaderStateSubject.next(true);
+    }
+
+    if (!loading) {
+      this.loadingSet.delete(url);
+
+    }
+
+    if (!this.loadingSet.size) {
+      this.loaderStateSubject.next(false);
+    }
   }
 
   get isVisibleLoader(): Observable<boolean> {
-    return this.loaderStateSubject.asObservable();
-  }
-
-  public hideLoader(): void {
-    this.loaderStateSubject.next(false);
-  }
-
-  public showLoader(): void {
-    this.loaderStateSubject.next(true);
+    return this.loaderStateSubject.asObservable().pipe(switchMap(result => result ? of(true).pipe(delay(300)) : of(false)));
   }
 }
